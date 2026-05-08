c53eb7cd6448555db945f8ebd4c11eeac8e9e046
jotaierre <jair.yhwh@gmail.com>
2026-05-08 18:49:35 -0300

    Ajuste de base path para o GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: './',
+  // Define a base como './' para que os links funcionem no GitHub Pages (subpastas)
   base: './',
+
   server: {
     port: 5173,
     open: true
   },
+
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: 'index.html',
-        menu: 'menu.html'
+        // Indica ao Vite que existem dois pontos de entrada (duas telas)
+        main: './index.html',
+        menu: './menu.html'
       }
     }
   }
